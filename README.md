b6038f9ff9225d592512f394ab61c723ca3cce2f
Benjamin Bouillot <benjamin.bouillot@indy.fr>
2024-06-24 14:32:35 +0200

    add parcel app + lint

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # English Test
+
 A small web app with vocabulary test english/french.
 
 Available [here](https://benio-b.github.io/english-test/)
--- app.js
+++ app.js
@@ -5,17 +5,17 @@ let isShowingAnswer = false;
 let isShowingEnglishQuestion;
 let currentQuestion;
 let questions = [...allQuestions];
+let numberQuestionsToDisplay = allQuestions.length;
 
 const questionElement = document.getElementById("question");
 const answerElement = document.getElementById("answer");
 const newQuestionButton = document.getElementById("new-question");
 const showAnswerButton = document.getElementById("show-answer");
-const appElement = document.getElementById("app");
-const questionContainerElement = document.getElementById('question-container');
+const questionContainerElement = document.getElementById("question-container");
 const categoryElement = document.getElementById("category");
-const categoriesButtonElement = document.getElementById('category-buttons');
-const noMoreQuestionsElement = document.getElementById('no-more-questions');
-const languageIndicatorElement = document.getElementById('language-indicator');
+const categoriesButtonElement = document.getElementById("category-buttons");
+const noMoreQuestionsElement = document.getElementById("no-more-questions");
+const languageIndicatorElement = document.getElementById("language-indicator");
 
 function showEndMessage() {
   noMoreQuestionsElement.style.display = "block";
@@ -24,24 +24,28 @@ function showEndMessage() {
 }
 
 function getUniqueCategories() {
-  return [...new Set(questions.map(item => item.category))];
+  return [...new Set(questions.map((item) => item.category))];
 }
 
 function getExplicitCategory(category) {
-  return questions.find((question) => question.category === category).explicitCategory;
+  return questions.find((question) => question.category === category)
+    .explicitCategory;
 }
 
 function filterQuestionsByCategory(category) {
   noMoreQuestionsElement.style.display = "none";
   questionContainerElement.style.display = "block";
 
-  if (category === 'all') {
+  if (category === "all") {
     questions = [...allQuestions];
     showNewQuestion();
     return;
   }
 
-  questions = [...allQuestions.filter((question) => question.category === category)]
+  questions = [
+    ...allQuestions.filter((question) => question.category === category),
+  ];
+  numberQuestionsToDisplay = questions.length;
   showNewQuestion();
 }
 
@@ -49,9 +53,9 @@ function addAllCategoriesButton() {
   const allCategories = getUniqueCategories();
   allCategories.forEach((category) => {
     const explicitCategory = getExplicitCategory(category);
-    const button = document.createElement('button');
-    button.setAttribute('data-category', category);
-    button.classList.add('category-button');
+    const button = document.createElement("button");
+    button.setAttribute("data-category", category);
+    button.classList.add("category-button");
     button.textContent = explicitCategory;
     categoriesButtonElement.appendChild(button);
     button.addEventListener("click", () => {
@@ -59,52 +63,63 @@ function addAllCategoriesButton() {
     });
   });
 
-  const buttonCategoryAll = document.getElementById('category-all');
+  const buttonCategoryAll = document.getElementById("category-all");
   buttonCategoryAll.addEventListener("click", () => {
-    filterQuestionsByCategory('all');
+    filterQuestionsByCategory("all");
   });
 }
 
 function showNewQuestion() {
-    if (questions.length === 0) {
-        showEndMessage();
-        return;
-    }
+  if (questions.length === 0) {
+    showEndMessage();
+    return;
+  }
 
-    isShowingEnglishQuestion = Math.random() < 0.5;
+  isShowingEnglishQuestion = Math.random() < 0.5;
 
-    currentQuestionIndex = Math.floor(Math.random() * questions.length);
-    currentQuestion = questions[currentQuestionIndex];
-    categoryElement.textContent = currentQuestion.explicitCategory;
-    
-    languageIndicatorElement.textContent = isShowingEnglishQuestion ? 'English:' : 'Français:';
-    languageIndicatorElement.classList.remove('language-fr');
-    languageIndicatorElement.classList.remove('language-en');
-    languageIndicatorElement.classList.add(isShowingEnglishQuestion ? 'language-en' : 'language-fr');
-    
-    questionElement.textContent = isShowingEnglishQuestion ? currentQuestion.english :  currentQuestion.french;
-    
+  currentQuestionIndex = Math.floor(Math.random() * questions.length);
+  currentQuestion = questions[currentQuestionIndex];
+  const numberRemainingQuestions =
+    numberQuestionsToDisplay - questions.length + 1;
+  categoryElement.textContent =
+    currentQuestion.explicitCategory +
+    ` (${numberRemainingQuestions}/${numberQuestionsToDisplay})`;
 
-    answerElement.textContent = " ";
-    answerElement.classList.add('answer-padding');
-    //answerElement.style.display = "none";
-    isShowingAnswer = false;
+  languageIndicatorElement.textContent = isShowingEnglishQuestion
+    ? "English:"
+    : "Français:";
+  languageIndicatorElement.classList.remove("language-fr");
+  languageIndicatorElement.classList.remove("language-en");
+  languageIndicatorElement.classList.add(
+    isShowingEnglishQuestion ? "language-en" : "language-fr",
+  );
 
-    showAnswerButton.style.display = "inline";
-    newQuestionButton.style.display = "none";
+  questionElement.textContent = isShowingEnglishQuestion
+    ? currentQuestion.english
+    : currentQuestion.french;
+
+  answerElement.textContent = " ";
+  answerElement.classList.add("answer-padding");
+  //answerElement.style.display = "none";
+  isShowingAnswer = false;
+
+  showAnswerButton.style.display = "inline";
+  newQuestionButton.style.display = "none";
 }
 
 function showAnswer() {
-    //answerElement.style.display = "block";
-    answerElement.classList.remove('answer-padding');
-    answerElement.textContent = isShowingEnglishQuestion ? currentQuestion.french : currentQuestion.english;
-    if (!isShowingAnswer) {
-        questions.splice(currentQuestionIndex, 1);   
-    }
-    console.log({questions})
-    isShowingAnswer = true;
-    showAnswerButton.style.display = "none";
-    newQuestionButton.style.display = "inline";
+  //answerElement.style.display = "block";
+  answerElement.classList.remove("answer-padding");
+  answerElement.textContent = isShowingEnglishQuestion
+    ? currentQuestion.french
+    : currentQuestion.english;
+  if (!isShowingAnswer) {
+    questions.splice(currentQuestionIndex, 1);
+  }
+  console.log({ questions });
+  isShowingAnswer = true;
+  showAnswerButton.style.display = "none";
+  newQuestionButton.style.display = "inline";
 }
 
 newQuestionButton.addEventListener("click", showNewQuestion);
